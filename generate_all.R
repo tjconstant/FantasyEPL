1b82d12cbf5a2320e820d694b0c218b5ce357f22
Tom Constant <t.j.constant@gmail.com>
2018-01-17 15:20:34 +0000

    abandon github page hosting model

--- generate_all.R
+++ generate_all.R
@@ -1,12 +1,11 @@
 source("generate_html.R")
 source("generate_feed.R")
 
-count <- read.table("counter.txt")
-count <- count + 1
-write.table(count, file = "counter.txt")
-
-commands <- c(paste0("git add ."),paste0("git commit"),paste0("git push"))
-
-for(i in 1:length(commands)) system(commands[i], wait = TRUE, show.output.on.console = TRUE)
-
-
+# count <- read.table("counter.txt")
+# count <- count + 1
+# write.table(count, file = "counter.txt")
+# 
+# commands <- c(paste0("git add ."),paste0("git commit"),paste0("git push"))
+# 
+# for(i in 1:length(commands)) system(commands[i], wait = TRUE, show.output.on.console = TRUE)
+# 
