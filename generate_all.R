9be81d69616e9658079ccf6b50872c01da2b3a3f
Tom Constant <t.j.constant@gmail.com>
2018-01-17 15:07:02 +0000

    auto_updating

--- generate_all.R
+++ generate_all.R
@@ -4,3 +4,11 @@ source("generate_feed.R")
 count <- read.table("counter.txt")
 count <- count + 1
 write.table(count, file = "counter.txt")
+
+command <- paste0("git add .")
+
+system(command)
+
+command2 <- paste0("git commit")
+
+system(command2)
